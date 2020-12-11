054e6da9d0dcaa2fb82f0f6784637ae9e02f28db
jiujiuwo <lihaifeng94@qq.com>
2020-12-11 23:16:54 +0800

    完成用户列表的展示，用户的添加，修改及删除

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -10,23 +10,21 @@
     <el-card class="box-card">
       <!-- 搜索区域 -->
       <el-row type="flex" :gutter="5">
-        <el-col :span="4">
+        <el-col :span="6">
           <el-input
             placeholder="请输入内容"
             class="input-with-select"
             :clearable="true"
             v-model="paramForPageQuery.query"
             @clear="queryPageData()"
-          ></el-input
+            ><el-button
+              class="input-search-btn"
+              icon="el-icon-search"
+              @click="queryPageData()"
+              clickable="true"
+              slot="append"
+            ></el-button></el-input
         ></el-col>
-        <el-col :span="1">
-          <el-button
-            icon="el-icon-search"
-            @click="queryPageData()"
-            clickable="true"
-            type="warning"
-          ></el-button>
-        </el-col>
         <el-col :span="2">
           <el-button type="primary" @click="addUserDialogVisible = true"
             >添加用户</el-button
@@ -59,15 +57,30 @@
           ></template>
         </el-table-column>
         <el-table-column prop="" label="操作">
-          <el-tooltip content="编辑" placement="top" :hide-after="1000">
-            <el-button type="primary" icon="el-icon-edit" circle></el-button>
-          </el-tooltip>
-          <el-tooltip content="删除" placement="top" :hide-after="1000">
-            <el-button type="danger" icon="el-icon-delete" circle></el-button>
-          </el-tooltip>
-          <el-tooltip content="分配角色" placement="top" :hide-after="1000">
-            <el-button type="warning" icon="el-icon-setting" circle></el-button>
-          </el-tooltip>
+          <template slot-scope="scope">
+            <el-tooltip content="编辑" placement="top" :hide-after="1000">
+              <el-button
+                type="primary"
+                icon="el-icon-edit"
+                circle
+                @click="showEditUserDialog(scope.row.id)"
+              ></el-button>
+            </el-tooltip>
+            <el-tooltip content="删除" placement="top" :hide-after="1000">
+              <el-button
+                type="danger"
+                icon="el-icon-delete"
+                circle
+                @click="deleteUserConfirm(scope.row.id)"
+              ></el-button>
+            </el-tooltip>
+            <el-tooltip content="分配角色" placement="top" :hide-after="1000">
+              <el-button
+                type="warning"
+                icon="el-icon-setting"
+                circle
+              ></el-button> </el-tooltip
+          ></template>
         </el-table-column>
         <el-table-column prop="mg_state" label="作用域插槽"
           ><template slot-scope="scope">{{ scope.row }}</template>
@@ -89,7 +102,7 @@
     <el-dialog
       title="添加用户"
       :visible.sync="addUserDialogVisible"
-      width="30%"
+      width="50%"
       @closed="addUserDialogClosed()"
     >
       <el-form
@@ -119,6 +132,39 @@
       <span slot="footer" class="dialog-footer">
         <el-button @click="addUserDialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="addUserConfirm">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- 修改用户信息对话框 -->
+    <el-dialog
+      title="修改用户信息"
+      :visible.sync="editUserDialogVisible"
+      @closed="editUserDialogClosed()"
+      width="50%"
+    >
+      <el-form
+        label-position="left"
+        label-width="80px"
+        :model="editUserFormVo"
+        :rules="rules"
+        ref="editUserFormRef"
+      >
+        <el-form-item label="用户名">
+          <el-input
+            v-model="editUserFormVo.username"
+            clearable
+            :disabled="true"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="editUserFormVo.email" clearable></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="editUserFormVo.mobile" clearable></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editUserDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editUserConfirm()">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -153,7 +199,9 @@ export default {
       totalSize: 0,
       paramForPageQuery: { query: '', pagenum: 1, pagesize: 10 },
       addUserDialogVisible: false, // 添加用户的弹出框
+      editUserDialogVisible: false, // 修改用户信息时的弹出框
       addUserFormVo: { username: '', password: '', email: '', mobile: '' },
+      editUserFormVo: { username: '', email: '', mobile: '' },
       // 表单的验证规则
       rules: {
         username: [
@@ -217,7 +265,6 @@ export default {
         // 添加成功
         this.$message.success(res.meta.msg)
         this.addUserDialogVisible = false
-        this.queryPageData()
       } else {
         this.$message.error(res.meta.msg)
       }
@@ -241,9 +288,80 @@ export default {
         }
       })
     },
-    // 关闭对话框后调用方法重置表单
+    // 修改用户的网络请求
+    async editUser() {
+      const { data: res } = await this.$http.put(
+        'users/' + this.editUserFormVo.id,
+        this.editUserFormVo
+      )
+      if (res.meta.status === 200) {
+        this.$message.success(res.meta.msg)
+      } else {
+        this.$message.error(res.meta.msg)
+      }
+      this.editUserDialogVisible = false
+    },
+    // 修改用户二次确认
+    editUserConfirm() {
+      this.$refs.editUserFormRef.validate((pass, obj) => {
+        if (pass) {
+          this.$confirm('确认修改？', '修改确认', {
+            confirmButtonText: '确认',
+            cancleButtonText: '取消'
+          })
+            .then(() => {
+              this.editUser()
+            })
+            .catch(() => {
+              this.$message.info('取消修改')
+              this.editUserDialogVisible = false
+            })
+        } else {
+          this.$message.error('表单信息有误')
+        }
+      })
+    },
+    // 关闭添加用户对话框后调用方法重置表单
     addUserDialogClosed() {
       this.$refs.addUserFormRef.resetFields()
+      this.queryPageData()
+    },
+    // 打开修改用户信息的对话框
+    async showEditUserDialog(userId) {
+      const { data: res } = await this.$http.get('users/' + userId)
+      if (res.meta.status !== 200) {
+        this.$message.error(res.meta.msg)
+      } else {
+        this.editUserFormVo = res.data
+        this.editUserDialogVisible = true
+      }
+    },
+    // 关闭修改用户信息对话框后调用方法
+    editUserDialogClosed() {
+      this.$refs.editUserFormRef.resetFields()
+      this.queryPageData()
+    },
+    // 删除用户确认
+    async deleteUserConfirm(userId) {
+      this.$confirm('确认删除用户' + userId, '删除确认', {
+        confirmButtonText: '确认',
+        cancleButtonText: '取消'
+      })
+        .then(() => {
+          this.deleteUser(userId)
+        })
+        .catch(() => {
+          this.$message.warning('取消删除')
+        })
+    },
+    async deleteUser(userId) {
+      const { data: res } = await this.$http.delete('users/' + userId)
+      if (res.meta.status === 200) {
+        this.$message.success(res.meta.msg)
+        this.queryPageData()
+      } else {
+        this.$message.error(res.meta.msg)
+      }
     }
   },
   created() {
@@ -262,7 +380,7 @@ export default {
 }
 .box-card {
   margin-left: 0px;
-  height: 98%;
+  height: 100%;
 }
 .el-table {
   margin-top: 20px;
@@ -270,5 +388,9 @@ export default {
 }
 .el-pagination {
   margin-top: 20px;
+}
+
+.input-search-btn:hover {
+  background-color: #bbbbbb;
 }
 </style>
